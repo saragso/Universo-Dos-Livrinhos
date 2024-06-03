7547cc521c62deef8a0c8a0dce070404a6bbf7a8
Sara Oliveira <saragmsoliveira@gmail.com>
2024-06-03 19:25:21 -0300

    edita o arquivo php reserveDadosLogin

--- reserveDadosLogin.php
+++ reserveDadosLogin.php
@@ -1,17 +1,10 @@
 <?php
 
-// Estabelecer conexão com o banco de dados
-$servername = "localhost"; // endereço do servidor
-$username = "seu_usuario"; // nome de usuário do banco de dados
-$password = "sua_senha"; // senha do banco de dados
-$dbname = "nome_do_banco_de_dados"; // nome do banco de dados
-
-// Criação da conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+include('conexao.php');
 
 // Verificar a conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+if ($conexao->connect_error) {
+    die("Conexão falhou: " . $conexao->connect_error);
 }
 
 // Receber dados do formulário
@@ -20,7 +13,7 @@ $senha = $_POST["password"];
 
 // Consulta SQL para verificar se o e-mail existe no banco de dados
 $sql = "SELECT * FROM usuarios WHERE email = '$email'";
-$result = $conn->query($sql);
+$result = $conexao->query($sql);
 
 if ($result->num_rows > 0) {
     // E-mail encontrado, verificar a senha
@@ -43,6 +36,6 @@ if (!empty($login_error)) {
 }
 
 // Fechar conexão com o banco de dados
-$conn->close();
+$conexao->close();
 
 ?>
