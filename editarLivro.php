6adedccbad83a96983fbafef20fc1e516ac0acf2
Sara Oliveira <saragmsoliveira@gmail.com>
2024-06-03 18:36:08 -0300

    edita o arquivo php editarLivro e altera um id na pagina catalogoLivro

--- editarLivro.php
+++ editarLivro.php
@@ -1,35 +1,29 @@
 <?php
 // Conectar ao banco de dados
-$servername = "seu_servidor";
-$username = "seu_usuario";
-$password = "sua_senha";
-$dbname = "biblioteca";
-
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+include('conexao.php');
 
 // Verificar conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Receber os dados do formulário
     $status = $_POST["status"];
-    $id_aluno = $_POST["id_aluno"];
+    $id_usuario = $_POST["id_usuario"];
     $data_emprestimo = $_POST["data_emprestimo"];
     $data_devolucao = $_POST["data_devolucao"];
 
     // Atualizar dados na tabela
-    $sql_update = "UPDATE emprestimos SET status='$status', data_emprestimo='$data_emprestimo', data_devolucao='$data_devolucao' WHERE id_emprestimo='$id_aluno'";
+    $sql_update = "UPDATE emprestimos SET status='$status', data_emprestimo='$data_emprestimo', data_devolucao='$data_devolucao' WHERE id_emprestimo='$id_usuario'";
     
-    if ($conn->query($sql_update) === TRUE) {
+    if ($conexao->query($sql_update) === TRUE) {
         echo '<div class="alert alert-success" role="alert">Registro atualizado com sucesso!</div>';
     } else {
-        echo '<div class="alert alert-danger" role="alert">Erro ao atualizar registro: ' . $conn->error . '</div>';
+        echo '<div class="alert alert-danger" role="alert">Erro ao atualizar registro: ' . $conexao->error . '</div>';
     }
 }
 
 // Fechar conexão com o banco de dados
-$conn->close();
+$conexao->close();
 ?>
