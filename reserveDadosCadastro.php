84f7bb947438746b86db6988e0d642df79de1756
Sara Oliveira <saragmsoliveira@gmail.com>
2024-06-03 19:05:09 -0300

    edita o arquivo php reserveDadosCadastro

--- reserveDadosCadastro.php
+++ reserveDadosCadastro.php
@@ -1,30 +1,24 @@
 <?php
-// Conectar ao banco de dados
-$servername = "seu_servidor";
-$username = "seu_usuario";
-$password = "sua_senha";
-$dbname = "seu_banco_de_dados";
 
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+include('conexao.php');
 
 // Verificar conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Receber os dados do formulário
-    $nome = strtoupper($_POST["firstname"]);
-    $sobrenome = strtoupper($_POST["lastname"]);
+    $nome_aluno = strtoupper($_POST["name"]);
+    $sobrenome_aluno = strtoupper($_POST["lastname"]);
     $email = strtoupper($_POST["email"]);
-    $celular = strtoupper($_POST["number"]);
+    $telefone_contato = strtoupper($_POST["number"]);
     $senha = strtoupper($_POST["password"]);
     $confirmasenha = strtoupper($_POST["confirmPassword"]);
 
     // Verificar se o e-mail já está cadastrado
-    $sql_email = "SELECT * FROM sua_tabela WHERE email = '$email'";
-    $result_email = $conn->query($sql_email);
+    $sql_email = "SELECT * FROM usuarios WHERE email = '$email'";
+    $result_email = $conexao->query($sql_email);
 
     if ($result_email->num_rows > 0) {
         echo '<div class="alert alert-danger" role="alert">Esse e-mail já está cadastrado.</div>';
@@ -36,17 +30,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // As senhas coincidem e o e-mail não está cadastrado, então podemos prosseguir com o cadastro
 
             // Inserir os dados no banco de dados (substitua 'sua_tabela' pelo nome da sua tabela)
-            $sql_insert = "INSERT INTO sua_tabela (nome, sobrenome, email, celular, senha) VALUES ('$nome', '$sobrenome', '$email', '$celular', '$senha')";
+            $sql_insert = "INSERT INTO usuarios (nome_aluno, sobrenome_aluno, email, celular, senha) VALUES ('$nome_aluno', '$sobrenome_aluno', '$email', '$celular', '$senha')";
             
-            if ($conn->query($sql_insert) === TRUE) {
+            if ($conexao->query($sql_insert) === TRUE) {
                 echo '<div class="alert alert-success" role="alert">Cadastro realizado com sucesso!</div>';
             } else {
-                echo '<div class="alert alert-danger" role="alert">Erro ao cadastrar: ' . $conn->error . '</div>';
+                echo '<div class="alert alert-danger" role="alert">Erro ao cadastrar: ' . $conexao->error . '</div>';
             }
         }
     }
 }
 
 // Fechar conexão com o banco de dados
-$conn->close();
+$conexao->close();
 ?>
