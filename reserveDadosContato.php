b28f25eec7de73179e46be07f116bf5f02cd47bf
Sara Oliveira <saragmsoliveira@gmail.com>
2024-06-03 19:14:37 -0300

    edita o arquivo php reserveDadosContato

--- reserveDadosContato.php
+++ reserveDadosContato.php
@@ -1,36 +1,30 @@
 <?php
-// Conectar ao banco de dados
-$servername = "seu_servidor";
-$username = "seu_usuario";
-$password = "sua_senha";
-$dbname = "seu_banco_de_dados";
 
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+include('conexao.php');
 
 // Verificar conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Receber os dados do formulário
-    $nome = strtoupper($_POST["firstname"]);
-    $sobrenome = strtoupper($_POST["lastname"]);
+    $nome_aluno = strtoupper($_POST["firstname"]);
+    $sobrenome_aluno = strtoupper($_POST["lastname"]);
     $email = strtoupper($_POST["email"]);
-    $celular = strtoupper($_POST["number"]);
+    $telefone_contato = strtoupper($_POST["number"]);
     $mensagem = strtoupper($_POST["mensagem-texto"]);
 
     // Inserir os dados no banco de dados (substitua 'sua_tabela' pelo nome da sua tabela)
-    $sql_insert = "INSERT INTO sua_tabela (nome, sobrenome, email, celular, mensagem) VALUES ('$nome', '$sobrenome', '$email', '$celular', '$mensagem')";
+    $sql_insert = "INSERT INTO sua_tabela (nome_aluno, sobrenome_aluno, email, telefone_contato, mensagem) VALUES ('$nome_aluno', '$sobrenome_aluno', '$email', '$telefone_contato', '$mensagem')";
     
-    if ($conn->query($sql_insert) === TRUE) {
+    if ($conexao->query($sql_insert) === TRUE) {
         echo "Mensagem salva! Entraremos em contato em breve";
     } else {
-        echo "Erro ao salvar mensagem: " . $conn->error;
+        echo "Erro ao salvar mensagem: " . $conexao->error;
     }
 }
 
 // Fechar conexão com o banco de dados
-$conn->close();
+$conexao->close();
 ?>
